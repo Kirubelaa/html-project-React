bac68e3d1be5b855974c61849dd0613c367a9d7b
Kirubelaa <KirubelAreaya00a@Gmail.Com>
2025-03-07 21:46:05 +0100

    feature-destop är imlmenterad

--- src/components/DisktopFeature/DisktopFeature.jsx
+++ src/components/DisktopFeature/DisktopFeature.jsx
@@ -5,27 +5,56 @@ const DisktopFeature = () => {
     <section className="extraFeature">
       <div className="container">
          <div className="extrafeature-sectionOne">
-           <h1>Make your money transfer simple and clear</h1>
-           <p>Banking transactions are free for you</p>
-           <p>No monthly cash commission</p>
-           <p>Manage payments and transactions online</p>
-           <span className="learn-more">learn more <i></i></span>
+          <div className="extrafeature-leftboxOne">
+              <div className="extrafeature-titelone">
+                  <h1>Make your money transfer simple and clear</h1>
+                  <ul className="feature-list">
+                    <li><i className="bi bi-check-circle"></i> Banking transactions are free for you</li>
+                    <li><i className="bi bi-check-circle"></i> No monthly cash commission</li>
+                    <li><i className="bi bi-check-circle"></i> Manage payments and transactions online</li>
+                  </ul>
+              </div>
 
+              <button className="learn-more">
+                <span> Learn more </span>
+                <i class="bi bi-arrow-right-short"></i> 
+              </button>
+          </div>
+         
+         
            <div className="sectionOne-image">
             <img src="./extraFeature-images/right-image.svg" alt="" />
            </div>
          </div>
 
          <div className="extrafeature-sectionTwo">
-         <h1>Make your money transfer simple and clear</h1>
-           <p>Banking transactions are free for you</p>
-           <p>No monthly cash commission</p>
-           <p>Manage payments and transactions online</p>
-           <span className="learn-more">learn more <i></i></span>
+            <div className="sectionTwo-image">
+                <img src="./extraFeature-images/left-image.svg" alt="" />
+            </div>
+            <div className="extrafeature-titeltwo">
+               <h1>Receive payment from international bank details</h1>
 
-           <div className="sectionOne-image">
-            <img src="./extraFeature-images/right-image.svg" alt="" />
-           </div>
+            <div className="extrafeature-rightboxTwo">
+                <div className="disktop-box">
+                   <img src="public/Feature-image/payments.svg" alt="" />
+                      <p>Manage your payments online. Mollis congue egestas egestas fermentum fames.</p>
+               </div>
+                
+                   <div className="disktop-box">
+                       <img src="public/Feature-image/cashback.svg" alt="" />
+                           <p>Manage your payments online. Mollis congue egestas egestas fermentum fames.</p>
+                   </div>
+            </div>
+            <button className="learn-more">
+                    <span> learn more </span>
+                    <i class="bi bi-arrow-right-short"></i> 
+                </button>
+           
+          </div>
+           
+          
+          
+        
          </div>
     </div>
   </section>
